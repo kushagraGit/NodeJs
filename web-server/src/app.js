b3d5f7778cf3b0edd7fd182dcdf3cccd3955fb53
EC2 Default User <ec2-user@ip-172-31-0-172.ap-southeast-1.compute.internal>
2019-07-04 13:08:51 +0000

    Stable commit of handlebars

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,13 +1,16 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 const publicDirectoryPath = path.join(__dirname,'../public')
-const viewPath = path.join(__dirname,'../templates')
+const viewPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
 
 app.set('view engine','hbs')
 app.set('views', viewPath)
 app.use(express.static(publicDirectoryPath))
+hbs.registerPartials(partialsPath)
 
 app.get('',(req,res) => {
     res.render('index',{
@@ -19,6 +22,7 @@ app.get('',(req,res) => {
 app.get('/help',(req,res) => {
     res.render('help',{
         title: 'Help',
+        helptext: 'This is help page',
         name: 'Kushagra'
     })
 })
@@ -54,6 +58,24 @@ app.get('/weather',(req,res)=>{
 //         age: 25
 //     })
 // })
+
+
+
+app.get('/help/*',(req,res) => {
+    res.render('notfound',{
+        title: 'Page Not Found',
+        name: 'Kushagra',
+        errorMessage: 'The help article you are looking for is not found.'
+    })
+})
+
+app.get('*',(req,res)=>{
+    res.render('notfound',{
+        title: 'Page Not Found',
+        name: 'Kushagra',
+        errorMessage: 'The page you are looking for is not found'
+    })
+})
 
 app.listen(8080, () => {
     console.log('Server is up')
